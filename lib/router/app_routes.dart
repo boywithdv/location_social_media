739c28ae714b9bc9fd73131245d886e023329847
tsukuda <jukiya8891@gmail.com>
2024-05-06 00:07:21 +0900

    ルートデータ追加

---
+++ lib/router/app_routes.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:location_social_media/view/pages/splash_page.dart';
+
+final rootNavigatorKey = GlobalKey<NavigatorState>();
+
+// スプラッシュのルードデータ
+@TypedGoRoute<SplashRouteData>(
+  path: SplashRouteData.path,
+)
+class SplashRouteData extends GoRouteData {
+  const SplashRouteData();
+  static const String path = '/';
+
+  @override
+  Widget build(BuildContext context, GoRouterState state) {
+    return const SplashPage();
+  }
+}
